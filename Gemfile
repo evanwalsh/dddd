5bf5a45cf55b589c39c11a070a24fceaa2a7310e
Evan Walsh <evan@massivedanger.com>
2012-10-01 19:54:13 -0400

    Stuff

--- Gemfile
+++ Gemfile
@@ -2,5 +2,7 @@ source "https://rubygems.org"
 
 gem "sinatra", "~> 1.3.3"
 
+gem "datamapper", "~> 1.2.0"
+
 gem "haml", "~> 3.1.7"
 gem "sass", "~> 3.2.1"
--- app.rb
+++ app.rb
@@ -1,5 +1,23 @@
 class App < Sinatra::Base
+  CHECK_REGEX = /^http:\/\/media.evanwalsh.net(.*?\.mp3)/
+
   get '/' do
-    'Hello World!'
+    @downloads = %w(One two THREE fOUR fIve)
+
+    haml :index
+  end
+
+  get '/track.*' do
+    is_valid = params[:url].match(CHECK_REGEX)
+
+    if is_valid
+      redirect params[:url]
+    else
+      redirect '/'
+    end
+  end
+
+  get '/app.css' do
+    sass :app
   end
 end
