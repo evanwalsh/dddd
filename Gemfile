e2501069c9f833d64c6c823a01527ad6c19dd28a
Evan Walsh <evan@massivedanger.com>
2012-10-01 20:28:53 -0400

    TRACK DLS. LAWL.

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source "https://rubygems.org"
 gem "sinatra", "~> 1.3.3"
 
 gem "datamapper", "~> 1.2.0"
+gem "dm-sqlite-adapter", "~> 1.2.0"
+gem "dm-postgres-adapter", "~> 1.2.0"
 
 gem "haml", "~> 3.1.7"
 gem "sass", "~> 3.2.1"
--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
+require 'data_mapper'
+
 class App < Sinatra::Base
   CHECK_REGEX = /^http:\/\/media.evanwalsh.net(.*?\.mp3)/
 
   get '/' do
-    @downloads = %w(One two THREE fOUR fIve)
+    @downloads = Download.all.group_by{ |d| d.url }.sort_by{|url, downloads| downloads.count }.reverse
 
     haml :index
   end
@@ -11,7 +13,9 @@ class App < Sinatra::Base
     is_valid = params[:url].match(CHECK_REGEX)
 
     if is_valid
-      redirect params[:url]
+      @download = create_download_for_url(params[:url])
+
+      redirect @download.url
     else
       redirect '/'
     end
@@ -20,4 +24,24 @@ class App < Sinatra::Base
   get '/app.css' do
     sass :app
   end
+
+  def create_download_for_url track_url
+    download = Download.new(url: track_url)
+    download.save
+
+    download
+  end
+
+  # Database stuff
+  DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/my.db")
+
+  class Download
+    include DataMapper::Resource
+    property :id, Serial
+    property :url, String
+    property :created_at, DateTime
+  end
+
+  DataMapper.finalize
+  DataMapper.auto_upgrade!
 end
